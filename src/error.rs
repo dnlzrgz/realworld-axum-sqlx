59f76a7768e7ae70838d04a20ded6249cffe4f3e
daniel <contact@dnlzrgz.com>
2026-06-27 21:28:45 +0200

    feat: add error types

---
+++ src/error.rs
@@ -0,0 +1,74 @@
+use axum::Json;
+use axum::http::{HeaderMap, HeaderValue, StatusCode, header::WWW_AUTHENTICATE};
+use axum::response::IntoResponse;
+use serde_json::json;
+use std::borrow::Cow;
+use std::collections::HashMap;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// Return `401 Unauthorized`
+    #[error("authentication required")]
+    Unauthorized,
+
+    /// Return `403 Forbidden`
+    #[error("current user is not allowed to perform that action")]
+    Forbidden,
+
+    /// Return `404 Not Found`
+    #[error("request path not found")]
+    NotFound,
+
+    /// Return `422 Unprocessable Entity`
+    #[error("error while processing the request body")]
+    UnprocessableEntity {
+        errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
+    },
+
+    /// Automatically return `500 Internal Server Error` on a `sqlx::Error`.
+    #[error("an error occurred with the database")]
+    Sqlx(#[from] sqlx::Error),
+
+    /// Return `500 Internal Server Error` on a `anyhow::Error`.
+    #[error("an internal error occurred")]
+    Anyhow(#[from] anyhow::Error),
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        let (status, message) = match &self {
+            Self::Unauthorized => {
+                // Include the `WWW-Authenticate` challenge required in the specification
+                // for the `401 Unauthorized` response code:
+                let mut headers = HeaderMap::new();
+                headers.insert(WWW_AUTHENTICATE, HeaderValue::from_static("Token"));
+                return (StatusCode::UNAUTHORIZED, headers, self.to_string()).into_response();
+            }
+            Self::Forbidden => (StatusCode::FORBIDDEN, self.to_string()),
+            Self::NotFound => (StatusCode::NOT_FOUND, self.to_string()),
+            Self::UnprocessableEntity { errors } => {
+                return (
+                    StatusCode::UNPROCESSABLE_ENTITY,
+                    Json(json!({"errors": errors})),
+                )
+                    .into_response();
+            }
+            Self::Sqlx(e) => {
+                tracing::error!("sqlx error: {:?}", e);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "internal server error".into(),
+                )
+            }
+            Self::Anyhow(e) => {
+                tracing::error!("internal error: {:#}", e);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "internal server error".into(),
+                )
+            }
+        };
+
+        (status, Json(json!({ "errors": { "body": [message] } }))).into_response()
+    }
+}
